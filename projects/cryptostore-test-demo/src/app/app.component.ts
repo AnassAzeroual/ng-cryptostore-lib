68af1dc5f7d2022c9d82c64f6871adab721c262b
anass azeroual <azeroual.anass2015@gmail.com>
2023-02-07 00:08:06 +0100

    add demo files

--- projects/cryptostore-test-demo/src/app/app.component.ts
+++ projects/cryptostore-test-demo/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { StorageService } from './../../../ng-cryptostore/src/lib/storage.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cryptostore-test-demo';
+export class AppComponent implements OnInit {
+  title = 'demo';
+  constructor(private srv: StorageService) { }
+  ngOnInit(): void {
+    this.srv.set('userData', [{ name: 'orange', icons: '🍊' }, { name: 'fraise', icons: '🍓' }, { name: 'banane', icons: '🍌' }], '2020')
+  }
 }
--- projects/cryptostore-test-demo/src/app/app.module.ts
+++ projects/cryptostore-test-demo/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StorageModule } from 'projects/ng-cryptostore/src/public-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocalstoreComponent } from './localstore/localstore.component';
+import { SessionstoreComponent } from './sessionstore/sessionstore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalstoreComponent,
+    SessionstoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StorageModule.withConfig({ storageType: "localStorage" })
   ],
   providers: [],
   bootstrap: [AppComponent]
